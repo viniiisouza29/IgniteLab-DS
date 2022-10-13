45aa9d522fd0e87f341d30406a0c57e527e3c698
vini <viniiisouza29@outlook.com>
2022-10-13 13:49:12 -0300

    login screen completion, storybook fix

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -1,11 +1,11 @@
 import * as CheckboxPrimite from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react'
 
-export interface CheckboxProps {}
+export interface CheckboxProps extends CheckboxPrimite.CheckboxProps {}
 
-export function Checkbox({}: CheckboxProps) {
+export function Checkbox(props: CheckboxProps) {
     return (
-        <CheckboxPrimite.Root className=" w-6 h-6 p-[2px] bg-gray-800 rounded">
+        <CheckboxPrimite.Root className=" w-6 h-6 p-[2px] bg-gray-800 rounded" {...props} >
             <CheckboxPrimite.Indicator asChild>
                 <Check weight="bold" className="h-5 w-5 text-cyan-500 align-center" />
             </CheckboxPrimite.Indicator>
